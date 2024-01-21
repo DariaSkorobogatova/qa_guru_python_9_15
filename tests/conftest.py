c08b17ed238f28847ea4b5fdd73d38204304ad17
Daria Skorobogatova <daria.skorobogatova@yandex.ru>
2024-01-21 13:54:33 +0300

    changed config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,54 +1,51 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene.support.shared import config
 from selene import browser
+from utils import attach
+from dotenv import load_dotenv
+import os
 
 
-# @pytest.fixture(scope='session', autouse=True)
-# def load_env():
-#     load_dotenv()
-
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_configs():
     browser.config.base_url = 'https://www.chitai-gorod.ru'
-    browser.config.timeout = 5.0
-    options = webdriver.ChromeOptions()
-    options.add_argument('--start-maximized')
-    browser.config.driver_options = options
-    yield
+    browser.config.timeout = 2.0
+    config.window_width = 1920
+    config.window_height = 1080
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
-    # browser.config.base_url = 'https://www.chitai-gorod.ru'
-    # browser.config.timeout = 2.0
-    # config.window_width = 1920
-    # config.window_height = 1080
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "100.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    #
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
-    #
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-    #     options=options
-    # )
-    #
-    # browser.config.driver = driver
-    #
-    # yield browser
-    #
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
-    #
-    # browser.quit()
-    #
+
