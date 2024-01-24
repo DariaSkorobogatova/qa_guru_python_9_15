49463a1ac9da3b05d243c49f30c18c5b019d93db
Daria Skorobogatova <daria.skorobogatova@yandex.ru>
2024-01-24 22:41:47 +0300

    tried to fix again clear cart test

--- chitai_gorod_suite/pages/cart_page.py
+++ chitai_gorod_suite/pages/cart_page.py
@@ -13,6 +13,8 @@ class CartPage:
         return self
 
     def clear_cart(self):
+        if browser.element('.change-city').wait_until(be.visible):
+            browser.element('.change-city__button--accept').click()
         browser.element('.app-title__append').should(be.visible)
         browser.element('.delete-many').click()
         return self
