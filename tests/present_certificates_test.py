a26480b86fd8120c2b943d7c7f6bc4f7939fde20
Daria Skorobogatova <daria.skorobogatova@yandex.ru>
2024-01-27 17:13:38 +0300

    check the rightness on pytest ini file

--- tests/present_certificates_test.py
+++ tests/present_certificates_test.py
@@ -1,18 +1,18 @@
-import allure
-from chitai_gorod_suite.pages.main_page import main_page
-from chitai_gorod_suite.pages.certificates_page import certificates
-
-
-@allure.title('Clear the cart test')
-def test_type_certificates():
-    with allure.step('Open main page'):
-        main_page.open()
-
-    with allure.step('Go to present certificate tab'):
-        certificates.click_present_certificates()
-
-    with allure.step('Assert certificates blocks are there'):
-        certificates.should_have_titles('Электронный сертификат', 'Пластиковый сертификат')
-
-
-
+# import allure
+# from chitai_gorod_suite.pages.main_page import main_page
+# from chitai_gorod_suite.pages.certificates_page import certificates
+#
+#
+# @allure.title('Clear the cart test')
+# def test_type_certificates():
+#     with allure.step('Open main page'):
+#         main_page.open()
+#
+#     with allure.step('Go to present certificate tab'):
+#         certificates.click_present_certificates()
+#
+#     with allure.step('Assert certificates blocks are there'):
+#         certificates.should_have_titles('Электронный сертификат', 'Пластиковый сертификат')
+#
+#
+#
